1d37f66218ae8ccd4717ad5172bf1048aad367c8
Daniel Monzon <monzond@hotmail.com.ar>
2024-11-25 18:34:41 -0300

    2024/11/25 modified

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static' )
 
 # MYSQL connection
 app.config['MYSQL_HOST'] = 'localhost'
@@ -15,7 +15,8 @@ app.config['MYSQL_DB'] = 'biblos12'
 mysql = MySQL(app)
 
 # Settings
-app.secret_key = 'mysecretkey' 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
+ 
 
 @app.route('/')
 def index():
@@ -23,7 +24,7 @@ def index():
 
 
 @app.route('/Books/books', methods=['GET', 'POST'])
-def books():
+def Books():
     try:
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM titles')
@@ -35,7 +36,7 @@ def books():
 
 
 @app.route('/Books/addbooks', methods=['GET','POST'])
-def addbooks():
+def addBooks():
     if request.method == 'POST':
         title = request.form['title']
         location = request.form['location']
@@ -44,7 +45,7 @@ def addbooks():
         cur.execute(
             'INSERT INTO titles (title, location, isbn) VALUES (%s, %s, %s)', (title, location, isbn))
         mysql.connection.commit()
-        return render_template('Books/books.html')
+        return redirect (url_for('Books'))
     else:
         return render_template('Books/addbooks.html')
 
@@ -95,6 +96,9 @@ def delete_book(id):
     except Exception as e:
         return f"Error: {str(e)}"
 
+
+# Authors
+
 @app.route('/Authors/authors', methods=['GET', 'POST'])
 def authors():
     try:
@@ -102,7 +106,7 @@ def authors():
         cur.execute('SELECT * FROM authors')
         data = cur.fetchall()
         print(data)  # For debugging purposes
-        return render_template('Authors/authors.html', titles=data)
+        return render_template('Authors/authors.html', authors=data)
     except Exception as e:
         return f"Error: {str(e)}"
 
@@ -114,9 +118,57 @@ def edit_author(id):
             'SELECT last_name, names, nationality FROM authors WHERE id=%s', (id,))
         data = cur.fetchone()
         mysql.connection.commit()
-        return render_template('Authors/edit_author.html', title=data)
+        return render_template('Authors/edit_author.html', author=data)
     except Exception as e:
-        return f"Error: {str(e)}"        
+        return f"Error: {str(e)}"  
+
+
+
+@app.route('/delete_author/<id>', methods=['GET', 'POST'])
+def delete_author(id):
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('DELETE FROM authors WHERE id=%s', (id,))
+        mysql.connection.commit()
+
+        cur.execute('SELECT * FROM authors')
+        data = cur.fetchall()
+        print(data)  # For debugging purposes
+
+        return render_template('Authors/authors.html', authors=data)
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+@app.route('/update/<id>', methods=['GET','POST'])
+def update_author(id):
+    if request.method == 'POST':
+        last_name = request.form['last_name']
+        names = request.form['names']
+        nationality = request.form['nationality']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+        UPDATE authors
+        SET last_name = %s,
+            names = %s,
+            nationality = %s
+        WHERE id = %s    
+        """, (last_name, names, nationality, id))
+        mysql.connection.commit()
+        return redirect(url_for("Authors/authors"))    
+    
+@app.route('/Authors/addauthor', methods=['GET','POST'])
+def addauthor():
+    if request.method == 'POST':
+        last_name = request.form['last_name']
+        names = request.form['names']
+        nationality = request.form['nationality']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'INSERT INTO authors (last_name, names, nationality) VALUES (%s, %s, %s)', (last_name, names, nationality))
+        mysql.connection.commit()
+        return redirect (url_for('authors'))
+    else:
+        return render_template('Authors/addauthor.html')
 
 
 
